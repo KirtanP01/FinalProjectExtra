5f871251e8df2ea4ac720173e058aaf9def94805
KirtanP01 <84253962+KirtanP01@users.noreply.github.com>
2022-03-21 23:31:30 -0500

    Added the platforms and the cutscene

---
+++ 3D Platformer-Mobile/Assets/Scripts/MainStory.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainStory : MonoBehaviour{
+    private void OnEnable(){
+        SceneManager.LoadScene("Level1", LoadSceneMode.Single);
+    }
+}
