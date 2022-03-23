a63b48e3082d597d8587b4ffda7b3d3607dae102
KirtanP01 <84253962+KirtanP01@users.noreply.github.com>
2022-03-22 23:25:55 -0500

    Changes

---
+++ 3D Isekai Mobile/Cutscene/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+
+    public Text nameText;
+    public Text dialogueText;
+
+    public Animator animator;
+
+    public static Queue<string> sentences;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        //Debug.Log("Starting conversation with " + dialogue.name);
+        animator.SetBool("IsOpen", true);
+        nameText.text = dialogue.name;
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        Debug.Log(sentences.Count);
+        if (sentences.Count == 0)
+        {
+            Debug.Log("End of list");
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        Debug.Log(sentence);
+        dialogueText.text = sentence;
+    }
+
+    void EndDialogue()
+    {
+        //Debug.Log("End of conversation.");
+        animator.SetBool("IsOpen", false);
+    }
+}
